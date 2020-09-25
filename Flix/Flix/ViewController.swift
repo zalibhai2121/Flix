9594052ad0b194bf90da615100f94e65aa529616
Zainab <zalibhai2121@gmail.com>
2020-09-25 15:53:02 -0500

    assignment 1

---
+++ Flix/Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Zainab Alibhai on 9/25/20.
+//  Copyright © 2020 Zainab Alibhai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
